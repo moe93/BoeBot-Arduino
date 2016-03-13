fe847bb90e75fadaaf3f3f51de9b5dc0c7a13b49
Moe Odeh <m.odeh93@gmail.com>
2016-03-13 17:11:44 -0400

    Version 0.2
    Expanded upon BoeBot's functionality

--- BoeBot_Arduino.ino
+++ BoeBot_Arduino.ino
@@ -1,5 +1,9 @@
 /* A recreation of Boe-Bot, an autonomous obstacle avoiding robot
    IR receiver: looking at the IR receiver from left to right, data, gnd, vin.
+   Created: 31/12/2015
+   Modified: 1/1/2016
+
+   ***FIRST FUNCTIONAL VERSION (to some degree)***
 */
 // ------------- Libraries and Global Variables ---------------
 
@@ -8,10 +12,9 @@
 Servo leftServo;
 Servo rightServo;
 int leftWhisker, rightWhisker;
-const long reverseDuration = 1500;
-const long rotationDuration = 500;
-unsigned long currentTime = millis();
-unsigned long previousTime = 0;
+const long reverseDuration = 500;
+const long rotationDuration = 250;
+unsigned long previousTime;
 
 
 // ---------------------- Setup ------------------------
@@ -30,47 +33,65 @@ void setup() {
 // ------------------------- Main program ------------------------
 void loop() {
 
-  leftWhisker = digitalRead(8);
-  rightWhisker = digitalRead(9);
+  leftWhisker = digitalRead(9);
+  rightWhisker = digitalRead(8);
   //Serial.print(leftWhisker);      //UNCOMMENT TO DEBUG
 
+  // ------------------ Drive Forward ------------------
   if ((leftWhisker != 0) && (rightWhisker != 0)) {
 
     // write both servos to move in one direction.
-    leftServo.write(0); // 0 is clockwise
-    rightServo.write(180); // 180 is counter clockwise
+    leftServo.write(180); // 180 is counter clockwise
+    rightServo.write(0); // 0 is clockwise
   }
 
+  // ------------------- rotate right --------------------
   else if ((leftWhisker == 0) && (rightWhisker != 0)) {
-    previousTime = currentTime;
-    while (currentTime < previousTime + reverseDuration) {
-      leftServo.write(180);
-      rightServo.write(0);
+
+    previousTime = millis();
+    while (millis() - previousTime < reverseDuration) {
+      leftServo.writeMicroseconds(1000);
+      rightServo.writeMicroseconds(2000);
     }
     delay(1000);
 
-    previousTime = currentTime;
-    while (currentTime < previousTime + rotationDuration) {
-      leftServo.write(180);
-      rightServo.write(180);
+    previousTime = millis();
+    while (millis() - previousTime < rotationDuration) {
+      leftServo.writeMicroseconds(2000);
+      rightServo.writeMicroseconds(2000);
     }
     delay(1000);
   }
 
-
+  // --------------------- Rotate left ------------------
   else if ((leftWhisker != 0) && (rightWhisker == 0)) {
-    previousTime = currentTime;
-    while (currentTime < previousTime + reverseDuration) {
-      leftServo.write(180);
-      rightServo.write(0);
+    previousTime = millis();
+    while (millis() - previousTime < reverseDuration) {
+      leftServo.writeMicroseconds(1000);
+      rightServo.writeMicroseconds(2000);
     }
     delay(1000);
-    previousTime = currentTime;
-    while (currentTime < previousTime + rotationDuration) {
-      leftServo.write(0);
-      rightServo.write(0);
+
+    previousTime = millis();
+    while (millis() - previousTime < rotationDuration) {
+      leftServo.writeMicroseconds(1000);
+      rightServo.writeMicroseconds(1000);
     }
     delay(1000);
+  }
+
+  // --------------- rotate left/right ----------------
+  else if ((leftWhisker == 0) && (rightWhisker == 0)) {
+    previousTime = millis();
+    while (millis() - previousTime < reverseDuration + 1000) {
+      leftServo.writeMicroseconds(1000);
+      rightServo.writeMicroseconds(2000);
+    }
+
+    while (millis() - previousTime < rotationDuration + 250) {
+      leftServo.writeMicroseconds(1000);
+      rightServo.writeMicroseconds(1000);
+    }
   }
 
 }
